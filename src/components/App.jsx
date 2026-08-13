b361750f4a8159764a3db689b4651edb64f5976b
Ethan <ethancredo.ascc@outlook.com>
2026-08-13 16:25:46 +0800

    fetch pokemon with useEffect and pass to main component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react"
+import Header from "./Header"
+import Main from "./Main"
+
 export default function App() {
+
+	const [pokemons, setPokemons] = useState([])
+
+	useEffect(() => {
+		fetch("https://pokeapi.co/api/v2/pokemon/1/")
+			.then((res) => res.json())
+			.then((json) => {
+				setPokemons(json)
+			})
+	}, [])
+	
+	console.log(pokemons)
+
 	return (
-		<h1>Hello world!</h1>
+		<>
+			<Header />
+			<Main pokemon={pokemons} />
+		</>
 	)
 }
