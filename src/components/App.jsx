39060a9929ecd5d825a718b511f821cf978b0493
Ethan <ethancredo.ascc@outlook.com>
2026-08-13 16:59:41 +0800

    TODO: display multiple pokemon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,37 @@ import Main from "./Main"
 export default function App() {
 
 	const [pokemons, setPokemons] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
 
 	useEffect(() => {
-		fetch("https://pokeapi.co/api/v2/pokemon/1/")
+		fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0/")
 			.then((res) => res.json())
 			.then((json) => {
 				setPokemons(json)
+				setIsLoading(false)
 			})
 	}, [])
-	
-	console.log(pokemons)
+
+	if (isLoading) {
+		return (
+			<div>
+				<h1>Loading...</h1>
+			</div>
+		)
+	} else {
+		console.log(pokemons.results)
+	}
 
 	return (
 		<>
 			<Header />
 			<Main pokemon={pokemons} />
+			{pokemons.results.map(poke => {
+				<>
+					<h2>Name: {poke.name}</h2>
+					{/* <img src={poke.sprites.front_default} alt="" /> */}
+				</>
+			})}
 		</>
 	)
 }
