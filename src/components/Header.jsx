bf414f61769cf361578b5f60e30714c81bd93ee3
Ethan <ethancredo.ascc@outlook.com>
2026-08-13 16:26:00 +0800

    create component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Memory Card Game</h1>
+
+            <span className="score">
+                <p>Current Score: </p>
+                <p>Best Score: </p>
+            </span>
+        </header>
+    )
+}
