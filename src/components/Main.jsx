bbd3bc264c3f54a21a7f4ded805cb5a2ee3a985a
Ethan Joseph <ethancredo.ascc@outlook.com>
2026-08-17 06:42:13 +0800

    display pokemons with card component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,7 +10,22 @@ function Gameboard({pokemon}) {
     return (
         <div className="gameboard">
             <h2>Game board</h2>
-            {/* <img src={pokemon.sprites.front_default} alt="" /> */}
+            {pokemon.map(poke => (
+                <Card key={poke.pokemon.id} poke={poke.pokemon} />
+			))}
+        </div>
+    )
+}
+
+function Card({poke}) {
+    const pokemonId = poke.id
+    const SPRITE_URL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png`
+
+    return (
+        <div className="pokemon-card">
+            <h2>{poke.name}</h2>
+            <p>{pokemonId}</p>
+            <img src={SPRITE_URL} alt={poke.name} />
         </div>
     )
 }
