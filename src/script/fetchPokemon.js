2285ac008d468da3adb910523e404b73b833e546
Ethan <ethancredo.ascc@outlook.com>
2026-08-17 09:57:38 +0800

    rename

---
+++ src/script/fetchPokemon.js
@@ -0,0 +1,36 @@
+function getRandomIds(count, max) {
+    const PokemonIds = Array.from({length: max}, (_, index) => index + 1);
+
+    // Fisher-Yates shuffle
+    for (let i = PokemonIds.length - 1; i > 0; i--) {
+        const r = Math.floor(Math.random() * (i + 1));
+        [PokemonIds[i], PokemonIds[r]] = [PokemonIds[r], PokemonIds[i]];
+    }
+
+    // console.log(ids)
+    return PokemonIds.slice(0, count)
+}
+
+export default async function fetchPokemon() {
+    const ids = getRandomIds(10, 151);
+
+    const response = await Promise.all(
+        ids.map(id => fetch(`https://pokeapi.co/api/v2/pokemon/${id}`))
+    )
+
+    if (response.some(response => !response.ok)) {
+        throw new Error('Failed to fetch Pokémon');
+    }
+
+    const result = await Promise.all(
+        response.map(res => res.json())
+    )
+
+    const pokemonCards = result.map(pokemon => ({
+        id: pokemon.id,
+        name: pokemon.name,
+        img: pokemon.sprites.other['official-artwork'].front_default,
+    }))
+
+    return pokemonCards
+}
