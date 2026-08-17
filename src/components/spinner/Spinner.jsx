6f7906221fb72dc212b0707fba20e589526c864b
Ethan <ethancredo.ascc@outlook.com>
2026-08-17 16:42:09 +0800

    spinner while loading

---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,9 @@
+import './spinner.css'
+
+export default function Spinner() {
+    return (
+        <div className='spinner'>
+            <span className="pokeball"></span>
+        </div>
+    )
+}
