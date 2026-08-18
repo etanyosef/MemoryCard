a341f1c98b937e177031d49826057264c7e3260a
Ethan <ethancredo.ascc@outlook.com>
2026-08-18 15:27:31 +0800

    add logo and how to

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
+import pokeball from '../assets/img/pokeball.png'
+
 export default function Header() {
     return (
         <header>
-            <h1>Memory Card Game</h1>
+            <div className='title'>
+                <img src={pokeball} alt="" />
+                <div>
+                    <span>Pokemon Master</span>
+                    <h1>Pokemon Memory</h1>
+                </div>
+            </div>
 
-            <span className="score">
-                <p>Current Score: </p>
-                <p>Best Score: </p>
+            <span className="howto">
+                <strong>How to play:</strong>
+                <p>
+                    Pick a Pokemon card only once! Each time you pick it will shuffle the cards.
+                </p>
             </span>
         </header>
     )
