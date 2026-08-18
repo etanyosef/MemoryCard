75c547c57ec73be301df7d2e5a406af0dde3c75f
Ethan <ethancredo.ascc@outlook.com>
2026-08-18 11:14:27 +0800

    add title

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -2,8 +2,11 @@ import './spinner.css'
 
 export default function Spinner() {
     return (
-        <div className='spinner'>
-            <span className="pokeball"></span>
-        </div>
+        <>
+            <div className='spinner'>
+                <span className="pokeball"></span>
+                <h2 className='loading'>Loading...</h2>
+            </div>
+        </>
     )
 }
